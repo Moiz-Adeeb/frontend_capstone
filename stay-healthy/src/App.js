d0411e39bfff7d204059640409fdb89ba87f4a82
Moiz-Adeeb <moizadeebofficial@gmail.com>
2025-07-08 12:06:18 +0500

    final

--- stay-healthy/src/App.js
+++ stay-healthy/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar/Navbar";
+import Landing_Page from "./Components/Landing_Page/Landing_Page";
+import Login from "./Components/Login/Login";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Set up BrowserRouter for routing */}
+      <BrowserRouter>
+        {/* Display the Navbar component */}
+        <Navbar />
+        {/* Set up the Routes for different pages */}
+        <Routes>
+          {/* Define individual Route components for different pages */}
+          <Route path="/" element={<Landing_Page />} />
+          {/* <Route path="/login" element={<Login />} /> */}
+          {/* <Route path="/signup" element={<SignUp />} /> */}
+          {/* <Route */}
+            {/* path="/instant-consultation" */}
+            {/* element={<InstantConsultation />} */}
+          {/* /> */}
+          <Route path="<component_route>" element={<component_name />} />{" "}
+          //Replace the component_route with the component path and
+          component_name with the component name as imported in the App.js file.
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
